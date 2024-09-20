132a5df85248a23108f428f898d38ca2dfbe5ed9
wgeesey <89809942+wgeesey@users.noreply.github.com>
2024-09-20 15:26:23 -0400

    Update styles.py

--- styles.py
+++ styles.py
@@ -1,9 +1,16 @@
 from tkinter import ttk
-from config import BACKGROUND_COLOR
+from config import BACKGROUND_COLOR  # Import the background color constant from config
 
 
 def configure_styles():
+    # Create a ttk.Style instance to manage styles for the tkinter widgets
     style = ttk.Style()
+    
+    # Configure the background color for frames
     style.configure('TFrame', background=BACKGROUND_COLOR)
+    
+    # Configure the background color for labels
     style.configure('TLabel', background=BACKGROUND_COLOR)
+    
+    # Configure the background color for entry widgets
     style.configure('TEntry', background=BACKGROUND_COLOR)
